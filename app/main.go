b871b4f6c0de3711d60647fcd4922ad433d2ccf3
luthfi55 <luthfi12354@gmail.com>
2023-08-14 12:26:57 +0700

    Add Collection and Request Feature

--- app/main.go
+++ app/main.go
@@ -4,7 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	db "github.com/jeksilaen/api-builder/db"
 	middlewares "github.com/jeksilaen/api-builder/middlewares"
-	handler "github.com/jeksilaen/api-builder/modules/user/handlers"
+	userHandler "github.com/jeksilaen/api-builder/modules/user/handlers"
+	collectionHandler "github.com/jeksilaen/api-builder/modules/collection/handlers"
+	requestHandler "github.com/jeksilaen/api-builder/modules/request/handlers"
 )
 
 func main() {
@@ -16,7 +18,9 @@ func main() {
 	router := gin.Default()
 	router.Use(middlewares.SetJSONContentTypeMiddleware())
 
-	handler.InitUserHttpHandler(router)
+	userHandler.InitUserHttpHandler(router)
+	collectionHandler.InitCollectionHttpHandler(router)
+	requestHandler.InitRequestHttpHandler(router)
 
 	router.Run("localhost:8080")
 }
